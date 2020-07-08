ab549c0436e8a034bdb59ed037f8ae62a51453ad
Peyman Ghasemi <p_gmtz@yahoo.com>
2020-07-09 04:26:43 +0430

    constants

---
+++ constants.py
@@ -0,0 +1,18 @@
+
+"""
+CONSTANTS
+"""
+
+INSURED_SHARE = 'سهم بیمه شده'
+INSURER_SHARE = 'سهم سازمان'
+COST = 'هزینه'
+PRESCRIPTION_NUM = 'تعداد نسخه'
+DRUG_USAGE = 'تعداد مصرفی از دارو'
+DRUG_NUM = 'تعداد اقلام دارو'
+DATE = 'تاریخ نسخه'
+PROVINCE = 'استان'
+NAME_IN_EXCEL = 'نام دارو در اکسل'
+NAME_IN_DATABASE = 'نام دارو در دیتابیس'
+BRAND_CODE = 'کد برند'
+GENERIC_CODE = 'کد ژنریک'
+DRUG_CODE = 'کد دارو'
